25adb04d05c705f3d614fd42965ed7c4763a867e
mardsonferreira <mardsonferreira25@gmail.com>
2021-03-21 13:08:50 -0300

    authenticating users

---
+++ backend/src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+    secret: '06355827d493b8aa23b878856166e7e7',
+    expiresIn: '7d',
+};
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,10 +1,14 @@
 import { Router }  from 'express';
 
+import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
+import authMiddleWare from './app/middlewares/auth';
 
 const routes = new Router();
 
-routes.get('/',(req, res) => {
-    return res.json({ok: true});
-});
+routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleWare);
 
 export default routes;
