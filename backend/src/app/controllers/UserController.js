5ccfb9d8395ebdff00e7c29867bf96792dd1f109
mardsonferreira <mardsonferreira25@gmail.com>
2021-03-21 13:53:19 -0300

    allow user add photo

--- backend/src/app/controllers/UserController.js
+++ backend/src/app/controllers/UserController.js
@@ -31,6 +31,55 @@ class UserController {
         });
     }
 
+    async update(req, res) {
+        const schema = Yup.object().shape({
+            name: Yup.string(),
+            email: Yup.string().email().required(),
+            oldPassword: Yup.string().min(6),
+            password: Yup.string()
+                .min(6)
+                .when('oldPassword', (oldPassword, field) =>
+                    oldPassword ? field.required() : field
+                ),
+            confirmPassword: Yup.string().when('password', (password, field) =>
+                password ? field.required().oneOf([Yup.ref('password')]) : field
+            ),
+        });
+
+        if (!(await schema.isValid(req.body))) {
+            return res.status(400).json({ error: 'Validations fails' });
+        }
+
+        const { email, oldPassword } = req.body;
+
+        const user = await User.findByPk(req.userId);
+
+        if (email !== user.email) {
+            const userExists = await User.findOne({
+                where: { email },
+            });
+
+            if (userExists) {
+                return res.status(401).json({ erro: 'Email already exists' });
+            }
+        }
+
+        if (oldPassword && !(await user.checkPassword(oldPassword))) {
+            return res
+                .status(401)
+                .json({ erro: 'Old password does not match' });
+        }
+
+        const { id, name, provider } = await user.update(req.body);
+
+        return res.json({
+            id,
+            name,
+            email,
+            provider,
+        });
+    }
+
 }
 
 export default new UserController();
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,14 +1,28 @@
 import { Router }  from 'express';
 
+import multer from 'multer';
+import multerConfig from './config/multer';
+
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
+
 import authMiddleWare from './app/middlewares/auth';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
 
 routes.use(authMiddleWare);
+
+/*
+* The following routes require the user to be authenticated
+*/
+
+routes.post('/files', upload.single('file'), FileController.store);
+
+routes.put('/users', UserController.update);
 
 export default routes;
