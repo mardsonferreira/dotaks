0ca9c8fbe5e1d472a7bb6d82bbc6d696af0f8176
mardsonferreira <mardsonferreira25@gmail.com>
2021-08-30 11:57:50 -0300

    Configuring eslint and creating initial routes

---
+++ front-end/src/pages/SignUp/index.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+// import { Container } from './styles';
+
+const SignUp: React.FC = () => {
+    return <h1> Sign Up</h1>
+}
+
+export default SignUp
---
+++ front-end/src/routes/index.tsx
@@ -0,0 +1,21 @@
+import React from "react"
+import { Switch, Route } from "react-router-dom"
+
+import SignIn from "../pages/SignIn"
+import SignUp from "../pages/SignUp"
+
+import Profile from "../pages/Profile"
+import Dashboard from "../pages/Dashboard"
+
+const Routes: React.FC = () => {
+    return (
+        <Switch>
+            <Route path="/" exact component={SignIn} />
+            <Route path="/register" component={SignUp} />
+            <Route path="/dashboard" component={Dashboard} />
+            <Route path="/profile" component={Profile} />
+        </Switch>
+    )
+}
+
+export default Routes
