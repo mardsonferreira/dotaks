74a226de787a37af839f64d2e841d74e52a12513
mardsonferreira <mardsonferreira25@gmail.com>
2021-09-07 07:41:42 -0300

    Add cors support

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,7 @@
 import 'dotenv/config';
 import express from 'express';
 import routes from './routes';
+import cors from 'cors';
 
 import './database';
 class App {
@@ -12,6 +13,7 @@ class App {
 
     middlewares() {
         this.server.use(express.json());
+        this.server.use(cors());
     }
 
     routes() {
