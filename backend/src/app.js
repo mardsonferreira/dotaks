f1a040cfba576a6c247a6b1c094aa57d7bfe199c
mardsonferreira <mardsonferreira25@gmail.com>
2021-09-14 08:25:28 -0300

    Add avatar info on session endpoint

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,6 +2,8 @@ import 'dotenv/config';
 import express from 'express';
 import routes from './routes';
 import cors from 'cors';
+import path from 'path';
+
 
 import './database';
 class App {
@@ -12,8 +14,12 @@ class App {
     }
 
     middlewares() {
-        this.server.use(express.json());
         this.server.use(cors());
+        this.server.use(express.json());
+        this.server.use(
+            '/files',
+            express.static(path.resolve(__dirname, '..', 'tmp', 'uploads'))
+        );
     }
 
     routes() {
